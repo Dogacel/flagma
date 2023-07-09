f82453dea5cd4d3bfe5db2461a86b2e1591ea33a
Doğaç Eldenk <dogacel@gmail.com>
2023-07-09 23:29:19 +0300

    agree terms

--- flagma-server/settings.gradle.kts
+++ flagma-server/settings.gradle.kts
@@ -10,6 +10,7 @@
 plugins {
     // Apply the foojay-resolver plugin to allow automatic download of JDKs
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.4.0"
+    id("com.gradle.enterprise") version ("3.13.4")
 }
 
 dependencyResolutionManagement {
@@ -24,6 +25,15 @@ dependencyResolutionManagement {
     }
 }
 
+gradleEnterprise {
+    if (System.getenv("CI") != null) {
+        buildScan {
+            publishAlways()
+            termsOfServiceUrl = "https://gradle.com/terms-of-service"
+            termsOfServiceAgree = "yes"
+        }
+    }
+}
 
 rootProject.name = "flagma-server"
 include("app")
